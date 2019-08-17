0beefc7c66e4c51a587298e0c89254143efab1fa
Julie Jonak <juliejonak10@gmail.com>
2019-08-17 11:24:37 -0700

    Cleans up and annotates code

--- graph_adventure/helperFunctions.py
+++ graph_adventure/helperFunctions.py
@@ -30,6 +30,12 @@ class Queue():
 
 
 def traverseMap(roomGraph, player):
+    """
+    Returns list of directions to move
+    
+    :param dict roomGraph: Graph of the world map to traverse  
+    :param player: Instance of the player class moving through the map   
+    """
     traversalPath = []
 
     # Create a map
@@ -62,7 +68,7 @@ def traverseMap(roomGraph, player):
             if player.currentRoom.w_to == None:
                 map[player.currentRoom.id]['w'] = None
 
-        print(f"In Room {player.currentRoom.id}: {map[player.currentRoom.id]}")
+        # print(f"In Room {player.currentRoom.id}: {map[player.currentRoom.id]}")
 
         if map[player.currentRoom.id]['n'] == '?':
             traversalPath.append('n')
@@ -79,7 +85,6 @@ def traverseMap(roomGraph, player):
             player.travel('s')
 
         elif map[player.currentRoom.id]['e'] == '?':
-            print(f" \n Moving east from {player.currentRoom.id} to room {player.currentRoom.e_to.id}. {roomGraph[player.currentRoom.id]} --> {roomGraph[player.currentRoom.e_to.id]} \n")
             traversalPath.append('e')
             map[player.currentRoom.id]['e'] = player.currentRoom.e_to.id
             last_move = 'e'
@@ -94,42 +99,42 @@ def traverseMap(roomGraph, player):
             player.travel('w')
 
         else:
-            # BFS to nearest ?
-            # append to traversalPath
-            # set currentRoom.id = room with ? exit
-            # loop
-            print("HIT THE ELSE")
+            # BFS to nearest unexplored exit and appends to traversalPath
             next_path = find_nearest_unexplored(player.currentRoom.id, roomGraph, map)
 
             # Adds shortest path to next unexplored to traversalPath
             # Moves player through those rooms
             for direction in next_path["path"]:
-                print(f"Trying to move {direction} from {player.currentRoom.id}")
+                # print(f"Trying to move {direction} from {player.currentRoom.id}")
                 player.travel(direction)
                 traversalPath.append(direction)
             
             # Updates map with newly explored rooms
             map = next_path["updated_map"]
-            
+
     return traversalPath
 
 
-# return dict with { "room": integer, "path": [path_to_next_room], updated_map: { map } }
+# returns dict with { "room": integer, "path": [path_to_next_room], updated_map: { map } }
+
 def find_nearest_unexplored(curr_room, graph, map):
+    """
+    Returns object { "room": integer, "path": list, updated_map: dict }
+    
+    :param int curr_room: Current room player is in  
+    :param dict graph: Graph of the world map to traverse  
+    :param dict map: Dictionary containing found rooms and exits  
+    """
     visited = {}
     q = Queue()
     q.enqueue( { "room": curr_room, "path": [] } )
-    print(f"Q: {q} \n")
     path_found = False
     chosen_path = None
 
     while not path_found:
         check = q.dequeue()
-        print(f"Top of while: Check: {check}")
         curr_path = check["path"]
         curr_room2 = check["room"]
-        # print(f"Checking room {curr_room2} in graph: {map}")
-        print(f"Checking room {curr_room2}")
 
         # check all directions of current room
         # if next room has "?", make new path
@@ -137,29 +142,22 @@ def find_nearest_unexplored(curr_room, graph, map):
         for key, value in map[curr_room2].items():
             # key: direction, value: room number
             if value == '?':
-                print(f"FOUND ? in room {curr_room2}: {key} {value}")
                 curr_path.append(key)
                 chosen_path = curr_path
                 path_found = True
                 break
-            print(f"checking {key}: {value}")
+
             if value != None and value not in visited:
                 path_copy = list(curr_path)
                 path_copy.append(key)
                 visited[value] = { "room": value, "path": path_copy }
-                print(f"new to append: {visited[value]}")
                 q.enqueue(visited[value])
-
-    print(f"Chosen path: {chosen_path}")
 
     for direction in chosen_path:
         # Sets the next room to be the direction we'll traverse in
-        print(f"Trying to move from {curr_room} {direction}")
         next_room = graph[curr_room][1][direction]
-        print(f"Moving {direction} from room {curr_room}. Next room is room {next_room}")
 
         if next_room not in map:
-            print(f"Room {next_room} not in map. Graph shows {next_room}'s exits are: {graph[next_room][1]}")
             # updates map[curr_room][direction] to next_room to mark as visited
             map[curr_room][direction] = next_room
             # Adds the next room to map as well
@@ -185,14 +183,9 @@ def find_nearest_unexplored(curr_room, graph, map):
             if direction == 'w':
                 map[next_room]['e'] = curr_room
             
-            print(f"Because we came from {direction}, room {next_room} stored as: {map[next_room]}")
             curr_room = next_room
-            print(f"\n New curr_room is room {curr_room}. Next room is {map[curr_room]}")
 
         else:
             curr_room = next_room
-            print(f"2 New curr_room is room {curr_room}: {map[curr_room]}")
 
-
-    # print({ "room": curr_room, "path": chosen_path, "map": map})
     return { "room": curr_room, "path": chosen_path, "updated_map": map}
